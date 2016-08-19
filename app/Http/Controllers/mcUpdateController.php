1ef45862e339e8d6ce7d40c7854e986da7fb8040
vr55 <vr5@bk.ru>
2016-08-19 16:32:07 +0300

    user agent added

--- app/Http/Controllers/mcUpdateController.php
+++ app/Http/Controllers/mcUpdateController.php
@@ -33,6 +33,12 @@ class mcUpdateController extends mcBaseController
 {
     /** @var \App\Models\mcSettings $settings */
     //private $settings;
+    $useragent = array(
+          'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+          'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0'
+    );
+
 
 /*------------------------------------------------------------------------------
 *
